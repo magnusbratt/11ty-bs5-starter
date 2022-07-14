3574d5f54f9d8b16be17de4072dabb660e872ae6
Magnus Bratt <magnus.bratt@mira.se>
2022-07-14 16:29:32 +0200

    Oklart

--- src/_assets/js/main.js
+++ src/_assets/js/main.js
@@ -1,20 +1,32 @@
 import AOS from 'aos';
 
+// import $ from "jquery";
 import "./_bootstrap.js";
-
-
+// import "slick-carousel";
+// import "//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"
 
 (function() {
-    document.addEventListener('DOMContentLoaded', () => {
-        const options = {
-            duration: 700,
-            easing: 'ease-out-quad',
-            once: true,
-            startEvent: 'load',
-            disable: 'mobile'
-        };
+    // document.addEventListener('DOMContentLoaded', () => {
+    //     const options = {
+    //         duration: 700,
+    //         easing: 'ease-out-quad',
+    //         once: true,
+    //         startEvent: 'load',
+    //         disable: 'mobile'
+    //     };
 
-        AOS.init(options);
+    //     AOS.init(options);
+    // });
+
+    $(document).ready(function() {
+        $('.logo-carousel').slick({
+            infinite: true,
+            slidesToShow: 6,
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 2000,
+            dots: true,
+        });
     });
 
 })();
